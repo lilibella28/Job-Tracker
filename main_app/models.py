0394d7bbcabac61f068435f9e7bf5beea4eff1b1
Jose Castro <jacastro023@gmail.com>
2022-04-29 12:30:09 -0500

    created photo model

--- main_app/models.py
+++ main_app/models.py
@@ -44,3 +44,11 @@ class Note(models.Model):
 
 	class Meta:
 		ordering = ['-date']
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+  
+
+    def __str__(self):
+        return f"Photo for application_id: {self.application_id} @{self.url}" 
