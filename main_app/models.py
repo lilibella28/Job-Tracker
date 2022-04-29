ff1498ecfe0c83b26414def8393aaec479a17d5f
Jose Castro <jacastro023@gmail.com>
2022-04-29 12:28:01 -0500

    added application model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
+SITES = (
+	('O', 'On-Site'),
+	('R', 'Remote'),
+	('H', 'Hybrid'),
+)
+# Create your models here.
+class Application(models.Model):
+	name = models.CharField(max_length=100)
+	role = models.CharField(max_length=100)
+	salary = models.CharField(max_length=250)
+	location = models.CharField(max_length=250)
+	site = models.CharField(
+		max_length=1,
+		#choices
+		choices=SITES,
+		default=SITES[0][0]
+	)
