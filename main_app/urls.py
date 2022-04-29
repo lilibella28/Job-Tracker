be9bb4a9e315d93d7499c4f85366c9b512ea0429
Jose Castro <jacastro023@gmail.com>
2022-04-29 12:34:10 -0500

    added photo and note url paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
   path('applications/create/', views.ApplicationCreate.as_view(), name='applications_create'),
   path('applications/<int:pk>/update/', views.ApplicationUpdate.as_view(), name='applications_update'),
   path('applications/<int:pk>/delete/', views.ApplicationDelete.as_view(), name='applications_delete'),
+  path('applications/<int:application_id>/add_note/', views.add_note, name='add_note'),
+  path('applications/<int:application_id>/add_photo/', views.add_photo, name='add_photo'),
 ]
