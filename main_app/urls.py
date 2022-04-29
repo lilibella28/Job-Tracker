95eadd79f96fd30cd512ea1f6976ebefe3da922a
Jose Castro <jacastro023@gmail.com>
2022-04-29 12:32:24 -0500

    added urls for index and details

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
+   path('about/', views.about, name='about'),
+    path('applications/', views.applications_index, name='index'),
+    path('applications/<int:application_id>/', views.applications_detail, name='detail'),
 ]
