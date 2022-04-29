17f925a238a89f423d58cdfdef48fefacc1d1493
GabrielCMM74 <97140818+GabrielCMM74@users.noreply.github.com>
2022-04-29 13:13:34 -0400

    Set up About/Home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Add the following import
-from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'about.html')
 
 def about(request):
     return render(request, 'about.html')
