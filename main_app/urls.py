c93eff197da466e47bd3039f59afd284ad38ac74
Jose Castro <jacastro023@gmail.com>
2022-04-29 12:33:28 -0500

    added create,update, and delete url

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-   path('about/', views.about, name='about'),
-    path('applications/', views.applications_index, name='index'),
-    path('applications/<int:application_id>/', views.applications_detail, name='detail'),
+  path('about/', views.about, name='about'),
+  path('applications/', views.applications_index, name='index'),
+  path('applications/<int:application_id>/', views.applications_detail, name='detail'),
+  path('applications/create/', views.ApplicationCreate.as_view(), name='applications_create'),
+  path('applications/<int:pk>/update/', views.ApplicationUpdate.as_view(), name='applications_update'),
+  path('applications/<int:pk>/delete/', views.ApplicationDelete.as_view(), name='applications_delete'),
 ]
