746d69e4e34cb380a7d8bc9118368585f607498b
Lilibeht Montano <65881308+lilibella28@users.noreply.github.com>
2023-01-13 16:26:51 -0500

    Updated allowed host to migrate deployment
    Updated allowed hosted to migrate project from Heroku to RailWay

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=prt9d-r#z6mn&1)ih#33%l0wk+wqh1p=f8r^$@9v4$_!c9y8p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://job-tracker-production.up.railway.app']
 
 
 # Application definition
